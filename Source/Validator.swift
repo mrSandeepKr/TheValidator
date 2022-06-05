ddd29ae02114a96274c1d2f1949b0c2412036d76
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-06-05 20:04:47 +0530

    commit

---
+++ Source/Validator.swift
@@ -0,0 +1,21 @@
+//
+//  Validator.swift
+//  TheValidator
+//
+//  Created by Sandeep Kumar on 05/06/22.
+//
+
+import Foundation
+
+public class ValidatorConfigObj {
+//    let lowerCaseAlphabetsCount: Int
+//    let upperCaseAlphabetsCount: Int
+//    let specialCharacterCount: Int
+//    let specialCharSet: [Character]
+}
+
+public class PasswordValidator {
+    func isValidPassword(_ password: String, config: ValidatorConfigObj? = nil) -> Bool {
+        return true
+    }
+}
