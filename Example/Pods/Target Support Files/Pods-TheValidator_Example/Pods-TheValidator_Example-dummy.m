87cbf161e90a4f98abef71b196cf0718f7df42c9
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-06-05 18:46:28 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-TheValidator_Example/Pods-TheValidator_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheValidator_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TheValidator_Example
+@end
---
+++ Example/Pods/Target Support Files/TheValidator/TheValidator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TheValidator : NSObject
+@end
+@implementation PodsDummy_TheValidator
+@end
